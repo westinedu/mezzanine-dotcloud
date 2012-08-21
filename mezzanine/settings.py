cd1a149878b184f074aeba83ec6722e2cdbdf567
Lucas Westine <westine@gmail.com>
2012-08-21 15:54:34 +0800

    add mezzanine

--- mezzanine/settings.py
+++ mezzanine/settings.py
@@ -336,10 +336,10 @@ DEBUG_TOOLBAR_CONFIG = {"INTERCEPT_REDIRECTS": False}
 # Allow any settings to be defined in local_settings.py which should be
 # ignored in your version control system allowing for settings to be
 # defined per machine.
-try:
-    from local_settings import *
-except ImportError:
-    pass
+#try:
+#    from local_settings import *
+#except ImportError:
+#    pass
 
 
 ####################
--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,7 @@
 import os
+import sys
+#sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__),'mezzanine')))
 os.environ['DJANGO_SETTINGS_MODULE'] = 'mezzanine.settings'
 import django.core.handlers.wsgi
 application = django.core.handlers.wsgi.WSGIHandler()
+
