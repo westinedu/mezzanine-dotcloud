9a0b729fe7f652ac1fe3b50af7595a5cd4488cd2
Lucas Westine <westine@gmail.com>
2012-08-28 16:17:27 +0800

    static files
    STATIC_ROOT = '/home/dotcloud/current/static/'
    works

--- mezzanineapp/settings.py
+++ mezzanineapp/settings.py
@@ -187,21 +187,25 @@ APP_DIRNAME = APP_ROOT.split(os.sep)[-1]
 # project specific.
 CACHE_MIDDLEWARE_KEY_PREFIX = PROJECT_DIRNAME
 
-# URL prefix for static files.
-# Example: "http://media.lawrence.com/static/"
-STATIC_URL = "/static/"
 
-# Additional locations of static files
-STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'mezzanineapp/static/'),
-)
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
 #STATIC_ROOT = os.path.join(PROJECT_ROOT, STATIC_URL.strip("/"))
-STATIC_ROOT = '/home/dotcloud/volatile/static/'
+STATIC_ROOT = '/home/dotcloud/current/static/'
+
+# URL prefix for static files.
+# Example: "http://media.lawrence.com/static/"
+STATIC_URL = "/static/"
+
+# Additional locations of static files
+#STATICFILES_DIRS = (
+#    os.path.join(PROJECT_ROOT, 'mezzanineapp/static/'),
+#)
+
+
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
